158e8488e86cde8ca928cc53271ae2c6e4df24f8
AliAhmed0000 <76479231+AliAhmed0000@users.noreply.github.com>
2023-04-07 21:28:17 +0200

    Delete A4988_Stepper_driver directory

--- A4988_Stepper_driver/A4988_interface.h
+++
@@ -1,7 +0,0 @@
-#ifndef A4988_INTERFACE_H_
-#define A4988_INTERFACE_H_
-void A4988_Init();
-void A4988_VoidStepRotation(char No_steps,char Direction);
-void A4988_VoidRevRotation(char No_revs,char Direction);
-
-#endif
